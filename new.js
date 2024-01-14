906ec7fb8f569ed8fb07bc8ab5419254df2f86c8
7suleyman <suleymannd@code.edu.az>
2024-01-14 12:55:23 +0400

    javatask

---
+++ new.js
@@ -0,0 +1,6 @@
+for(var i = 0;i<=10;i++) {
+    if(i==0||i==2||i==4||i==6||i==8||i==10){
+        continue;
+    }
+    document.writeln(i);
+}
---
+++ newjs.js
@@ -0,0 +1,4 @@
+for(var i = 0;i<=25;i++) {
+    document.writeln(i);
+}
+
